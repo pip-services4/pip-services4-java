188a26faa30c793f62e699f8e150e2b9ef1f6603
Danil Prisiazhnyi <judas.priest999@gmail.com>
2023-11-21 15:56:15 +0300

    clean SecurityCtx and Principal

--- pip-services4-http-java/src/org/pipservices4/http/auth/PipSecurityContext.java
+++ pip-services4-http-java/src/org/pipservices4/http/auth/PipSecurityContext.java
@@ -9,18 +9,22 @@ import java.util.List;
 
 public class PipSecurityContext implements SecurityContext {
     private UserPrincipal _user;
-    private Object _session;
-    private ContainerRequestContext _requestContext;
+    private List<String> _roles;
+    private String _authType;
 
-    public PipSecurityContext(UserPrincipal user, ContainerRequestContext requestContext) {
+    private boolean _isSecure = false;
+
+    public PipSecurityContext(UserPrincipal user, List<String> roles) {
         _user = user;
-        _requestContext = requestContext;
+        _roles = roles;
+        _authType = SecurityContext.BASIC_AUTH;
     }
 
-    public PipSecurityContext(UserPrincipal user, Object session, ContainerRequestContext requestContext) {
+    public PipSecurityContext(UserPrincipal user, List<String> roles, String authType, boolean isSecure) {
         _user = user;
-        _session = session;
-        _requestContext = requestContext;
+        _roles = roles;
+        _authType = authType;
+        _isSecure = isSecure;
     }
 
     @Override
@@ -30,26 +34,53 @@ public class PipSecurityContext implements SecurityContext {
 
     @Override
     public boolean isUserInRole(String role) {
-        List<String> roles = _user != null ? _user.getProperty("roles") != null ?
-                (List<String>) _user.getProperty("roles") : new ArrayList<>() : new ArrayList<>();
-        return !roles.isEmpty() && roles.contains(role);
+        return !_roles.isEmpty() && _roles.contains(role);
     }
 
     @Override
     public boolean isSecure() {
-        return _requestContext.getSecurityContext().isSecure();
+        return _isSecure;
     }
 
     @Override
     public String getAuthenticationScheme() {
-        return _requestContext.getSecurityContext().getAuthenticationScheme();
+        return _authType;
     }
 
-    public Object getSession() {
-        return _session;
-    }
 
-    public void setSession(Object session) {
-        _session = session;
+
+    // Example of creating security context in facade
+    public void loadSession(ContainerRequestContext req) {
+        class UserObj { // obj from pip lib
+            List<String> Roles;
+        }
+        class Session { // obj from pip lib
+            String UserId;
+            String UserName;
+            UserObj User;
+            String Id;
+        }
+
+        // Get somewhere session
+        var session = new Session();
+
+        req.setProperty("user_id", session.UserId);
+        req.setProperty("user_name", session.UserName);
+        req.setProperty("user", session.User);
+        req.setProperty("session_id", session.Id);
+
+        var origCtx = req.getSecurityContext();
+        var roles = session.User.Roles;
+
+        var secCtx = new PipSecurityContext(
+                new UserPrincipal("username"),
+                roles,
+                origCtx.getAuthenticationScheme(),
+                origCtx.isSecure()
+        );
+
+        req.setSecurityContext(secCtx);
+
+        // next processing methods
     }
 }
--- pip-services4-http-java/src/org/pipservices4/http/auth/UserPrincipal.java
+++ pip-services4-http-java/src/org/pipservices4/http/auth/UserPrincipal.java
@@ -6,8 +6,6 @@ import java.util.Map;
 
 public class UserPrincipal implements Principal {
     private String _userName;
-    private String _userId;
-    private Map<String, Object> _props;
 
 
     public UserPrincipal(String userName) {
@@ -21,61 +19,5 @@ public class UserPrincipal implements Principal {
 
     public void setName(String userName) {
         _userName = userName;
-    }
-
-    public String getUserId() {
-        return _userId;
-    }
-
-    public void setUserId(String userId) {
-        _userId = userId;
-    }
-
-    public Map<String, Object> getProps() {
-        return _props;
-    }
-
-    public void setProps(Map<String, Object> props) {
-        if (props != null && !props.keySet().isEmpty()) {
-            props.keySet().forEach(k -> this.setProperty(k, props.get(k)));
-        }
-    }
-
-    public void setProps(Map<String, Object> props, boolean replaceAll) {
-        if (props != null && !props.keySet().isEmpty()) {
-            if (replaceAll) {
-                _props = new HashMap<>(props);
-            } else props.keySet().forEach(k -> this.setProperty(k, props.get(k)));
-        }
-    }
-
-    public Object getProperty(String key) {
-        if (_props == null || !_props.containsKey(key)) {
-            return null;
-        }
-        return _props.get(key);
-    }
-
-    public void setProperty(String key, Object value) {
-        if (_props == null) {
-            _props = new HashMap<>();
-        }
-        if (_props.containsKey(key)) {
-            _props.replace(key, value);
-        } else {
-            _props.put(key, value);
-        }
-    }
-
-    public void removeProperty(String key) {
-        if (_props != null) {
-            _props.remove(key);
-        }
-    }
-
-    public void clearProps() {
-        if (_props == null) {
-            _props = new HashMap<>();
-        } else _props.clear();
     }
 }
